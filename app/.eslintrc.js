e6b34371177f45f4e487c01787bc143a9b47b547
Peter Lu <95131783+Mitrofan2001@users.noreply.github.com>
2024-03-18 21:06:17 +0800

    feat: initial frontend and install prettier, eslint
    * project init
    
    * feat: env setting
    
    ---------
    
    Co-authored-by: OwenLu <gg0125gg@gmail.com>

---
+++ app/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: ['next/core-web-vitals', 'prettier', 'plugin:tailwindcss/recommended'],
+  plugins: ['tailwindcss'],
+  settings: {
+    tailwindcss: {
+      callees: ['cn', 'cva']
+    }
+  },
+  rules: {
+    'react/jsx-curly-brace-presence': ['error', { props: 'always', children: 'always', propElementValues: 'always' }],
+    'tailwindcss/no-custom-classname': 'off',
+    'tailwindcss/classnames-order': 'error'
+  }
+}
