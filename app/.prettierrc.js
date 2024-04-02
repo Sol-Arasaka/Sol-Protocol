896812aeb1398829d9ca4c2d8261008dde830c69
Mitrofan2001 <peterlu900203@gmail.com>
2024-04-02 19:11:38 +0800

    fix: resovle merge problem

--- app/.prettierrc.js
+++ app/.prettierrc.js
@@ -1,33 +1,32 @@
 /** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 module.exports = {
-    endOfLine: 'lf',
-    printWidth: 120,
-    semi: false,
-    singleQuote: true,
-    tabWidth: 2,
-    trailingComma: 'none',
-    plugins: ['@ianvs/prettier-plugin-sort-imports'],
-    importOrder: [
-      '<BUILT_IN_MODULES>',
-      '^(react/(.*)$)|^(react$)',
-      '^(next/(.*)$)|^(next$)',
-      '<THIRD_PARTY_MODULES>',
-      '',
-      '^core/(.*)$',
-      '^@/api/(.*)$',
-      '^@/constants/(.*)$',
-      '^@/types/(.*)$',
-      '^@/lib/(.*)$',
-      '^@/utils/(.*)$',
-      '^@/hooks/(.*)$',
-      '^@/store/(.*)$',
-      '^@/pages/(.*)$',
-      '^@/components/base$',
-      '^@/components/(.*)$',
-      '',
-      '^[./]'
-    ],
-    importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
-    importOrderTypeScriptVersion: '5.1.6'
-  }
-  
+  endOfLine: 'lf',
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  importOrder: [
+    '<BUILT_IN_MODULES>',
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^core/(.*)$',
+    '^@/api/(.*)$',
+    '^@/constants/(.*)$',
+    '^@/types/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/utils/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/store/(.*)$',
+    '^@/pages/(.*)$',
+    '^@/components/base$',
+    '^@/components/(.*)$',
+    '',
+    '^[./]'
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.1.6'
+}
