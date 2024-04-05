f038c78a78fa98b9250cf7711c36b2464be9058f
OwenLu <gg0125gg@gmail.com>
2024-04-05 20:23:00 +0800

    feat: Add Solana functionality and update UI

--- app/src/pages/test/index.tsx
+++ app/src/pages/test/index.tsx
@@ -1,51 +1,69 @@
+import { useState } from 'react';
 import { Button } from '@/components/base'
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { getProposePDA, getAssertPDA, getChallengeAssertPDA } from '@/utils/anchor'
+import { getProposePDA, getAssertPDA, getChallengeAssertPDA, SolProtocol, programID } from '@/utils/anchor'
+import { useSolanaProvider } from '@/hooks/solanaProvider';
+import { Program } from '@coral-xyz/anchor';
+import idl from "@/utils/idl.json";
+import { BN } from '@coral-xyz/anchor';
+
 
 
 const TestPage = () => {
   const { publicKey, connected, sendTransaction } = useWallet();
+  const provider = useSolanaProvider();
+  const [isLoading, setIsLoading] = useState(false);
+  const [proposalId, setProposalId] = useState<number>(0);
+  const [assertId, setAssertId] = useState<number>(0);
+  const [challengeId, setChallengeId] = useState<number>(0);
+  const [proposalName, setProposalName] = useState<string>("");
+  const [amount, setAmount] = useState<string>("");
 
 
+  const onMoveFunds = async (type: "propose" | "assert" | "challengeAssert") => {
+    if (!provider || !publicKey || !sendTransaction) return;
+
+    // The program variable is an instance of the Program class.
+    // It takes our program's idl and the programId as arguments.
+    // Using the idl it generates a set of methods that can be called on the program.
+    // The programID is the public key of the program. It is used to interact with the program on-chain.
+    const program = new Program<SolProtocol>(idl as any, programID, provider);
+
+    setIsLoading(true);
+
+    try {
+      const proposalAccount = getProposePDA({ publicKey, proposalId });
+      const assertAccount = getAssertPDA({ publicKey, proposalId, assertId });
+      const challengeAccount = getChallengeAssertPDA({ publicKey, proposalId, challengeId });
+
+      // The sig variable is the transaction signature.
+      // It is used to track the transaction on-chain.
+      let signature: string | undefined;
+
+      if (type === "propose") {
+        signature = await program.methods.propose(new BN(proposalId), proposalName, new BN(amount))
+          .accounts({
+            authority: publicKey,
+            proposalAccount,
+            systemProgram: publicKey,
+          })
+          .rpc(); // The rpc method sends the transaction to the cluster and returns the transaction signature.
+      }
+      console.log("Transaction Signature: ", signature);
+
+      // After the transaction is sent we update the balances of the user and the vault.
+    } catch (err) {
+      console.log("Transaction Error: ", err);
+    }
+    setIsLoading(false);
+  };
   return (
     <>
       <div className={"mt-6 flex flex-col gap-4"}>
         <Button
-          onClick={() => {
-            try {
-              const proposePDA = getProposePDA(publicKey!);
-              console.log(proposePDA);
-            } catch (err) {
-              console.log('getProposePDA filed', err);
-            }
-          }}
+          onClick={() => onMoveFunds("propose")}
         >
-          {"getProposePDA"}
-        </Button>
-        <Button
-          onClick={() => {
-            try {
-              const proposePDA = getAssertPDA(publicKey!);
-              console.log(proposePDA);
-            }
-            catch (err) {
-              console.log('getProposePDA filed', err);
-            }
-          }}
-        >
-          {"getAssertPDA"}
-        </Button>
-        <Button
-          onClick={() => {
-            try {
-              const proposePDA = getChallengeAssertPDA(publicKey!);
-              console.log(proposePDA);
-            } catch (err) {
-              console.log('getChallengeAssertPDA filed', err);
-            }
-          }}
-        >
-          {"getChallengeAssertPDA"}
+          {"propose"}
         </Button>
       </div>
     </>
